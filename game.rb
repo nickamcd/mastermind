f9dcd1be8316b584ee167a4b5e1d21f6b551f969
Nick McDaniel <nickamcd@gmail.com>
2020-12-16 16:29:53 -0800

    Added beginning logic for starting a game.

--- game.rb
+++ game.rb
@@ -1,3 +1,43 @@
-class Game
+require './player'
 
+class Game
+  TURN_TOTAL = 12
+  rounds = -1
+
+  def start_game
+    # begin game by asking for amount of rounds
+    loop do
+      puts 'Please enter an even number of rounds to play'
+
+      rounds = gets.chomp.to_i
+
+      break if rounds % 2 == 0
+    end
+
+    puts "number of rounds: #{rounds}"
+
+    puts 'enter player name'
+
+    player_name = gets.chomp
+
+    player_1 = Player.new(player_name)
+
+    puts player_1.name
+
+    computer_player = Player.new('Computer')
+
+    computer_code = computer_player.generate_rand_code
+  end
+
+  def play_round
+    turn_count = 0
+
+    while turn_count < TURN_TOTAL
+      player_1.make_guess
+
+      turn_count += 1
+    end
+
+    
+  end
 end
--- player.rb
+++ player.rb
@@ -16,28 +16,22 @@ class Player
       puts 'Please enter a code made up of the 4 of the following peg colors'
       p CodePeg::VALID_COLORS
 
+      #get each peg color from the user
       4.times do |i|
         puts "Enter peg #{i + 1}"
 
         @code[i] = gets.chomp.strip
       end
 
+      #check the validity of user input
       valid = Player.is_valid_code?(@code)
-
-      p @code
     end
 
     p @code
   end
-
-
 
   # check validity of user code
   def self.is_valid_code?(input_arr)
     input_arr.all? { |element| CodePeg::VALID_COLORS.include?(element.downcase) }
   end
 end
-
-test_player = Player.new('test')
-
-test_player.create_code
